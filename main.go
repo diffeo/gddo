a75481162cb48a4babf3fb18b7e8760148050596
kujenga <ataylor0123@gmail.com>
2015-12-17 21:36:21 -0500

    properly configured base handler to proxy when possible

--- main.go
+++ main.go
@@ -4,29 +4,43 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 
-	"golang.org/x/net/context"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
 )
+
+func init() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/github.com/rastech") {
+			handleOurs(w, r)
+		} else {
+			handleUpstream(w, r)
+		}
+	})
+}
+
+// handle requests ourselves
+
+func handleOurs(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Coming soon: our private repositories!"))
+}
+
+// proxy requests to godoc.org
 
 var gddoURL = &url.URL{
 	Scheme: "https",
 	Host:   "godoc.org",
 }
 
-func gddoProxy(ctx context.Context) *httputil.ReverseProxy {
+func handleUpstream(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
+	log.Infof(ctx, "proxying to upstream")
+
 	proxy := httputil.NewSingleHostReverseProxy(gddoURL)
 	proxy.Transport = urlfetch.Client(ctx).Transport
-	return proxy
-}
 
-func init() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ctx := appengine.NewContext(r)
-		proxy := gddoProxy(ctx)
-
-		proxy.ServeHTTP(w, r)
-	})
+	proxy.ServeHTTP(w, r)
 }
